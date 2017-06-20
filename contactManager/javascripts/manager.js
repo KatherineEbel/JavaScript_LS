3833daeae4e6700698d803f62fcfc216b3821562
KatherineEbel <kathyebel@me.com>
2017-06-20 16:03:26 -0400

    Complete implementing contact manager with flex-box

--- contactManager/javascripts/manager.js
+++ contactManager/javascripts/manager.js
@@ -1,35 +1,42 @@
-$UIElements = {
+let contact = {
+id: 0,
+name: '',
+email: '',
+phone: '',
+tag: ''
+};
+
+let $UIElements = {
   templates: $('[type="text/x-handlebars-template"]'),
   search: $('#search'),
-  searchEmptyMessage: $('#search-empty'),
   formContainer: $('#form-container'),
-  tagsContainer: $('.tags ul'),
-  formName: $('.form-name'),
+  tagsContainer: $('.tags'),
   contacts: $('#contacts'),
-  contactElementHeight: 0,
-  getFormName() {
-    this.formName = $('.form-name');
-    return this.formName.text();
+  searchEmptyMessage(searchVal){
+     $('#search-empty')
+       .text(searchVal.length === 0 ? '' :`There are no contacts containing ${searchVal}`);
+  },
+  getFormTitle() {
+    return $('.form-name').text();
+  },
+  setFormTitle(title) {
+    $('.form-name').text(title);
   },
   isFormActive() {
     return !this.formContainer.hasClass('collapsed');
   },
-  setEmptyMessage(searchValue) {
-    this.searchEmptyMessage = $('#search-empty');
-    this.searchEmptyMessage.text(`There are no contacts with search value ${searchVal}`);
+  addTag(name) {
+    this.tagsContainer.find('ul').append(`<li><a class="button" href="#">${name}</a></li>`)
   },
-  getContactElementHeight() {
-    return this.contacts.outerHeight();
-  },
-  setContactElementHeight() {
-    this.contactElementHeight = this.getContactElementHeight();
+  getSelectedTagNames() {
+    return $('.selected').map((_, el) => $(el).text()).get();
   }
 };
 
 function ContactManager(state) {
   this.currentId = state === null ? 0 : state.currentId;
   this.contacts = state === null ? [] : state.contacts;
-  this.tags = state === null ? ['Family', 'Co-Workers', 'Friends'] : state.tags;
+  this.tags = state === null ? ['None'] : state.tags;
   this.compileTemplates();
   this.renderContacts();
   this.init();
@@ -52,23 +59,40 @@ ContactManager.prototype.compileTemplates = function() {
 
 ContactManager.prototype.renderContacts = function() {
   $UIElements.contacts.html(this.templates.contactsTemplate({contacts: this.contacts}));
-  $UIElements.setContactElementHeight();
 };
 
 ContactManager.prototype.init = function() {
   this.bind();
+  $UIElements.tagsContainer.html(this.templates.tagsTemplate({tags: this.tags}));
+
 };
 
+ContactManager.prototype.saveContact = function(data) {
+  let updated = data.reduce((c, obj) => {
+    c[obj.name] = obj.name === 'id' ? +obj.value : obj.value;
+    return c;
+  }, Object.create(contact));
+  let thisContact = this.getContact(updated.id);
+  if (thisContact === -1) {
+    updated.id = this.getNextId();
+    this.contacts.push(updated);
+  } else {
+    this.contacts.splice(this.contacts.indexOf(thisContact), 1, updated);
+  }
+}
 // bind events to manager
 ContactManager.prototype.bind = function() {
   $(document).on('submit', e => {
     e.preventDefault();
-    let formName = $('.form-name').text();
+    let formName = $UIElements.getFormTitle();
     if (formName === 'Add Tag') {
       this.addTag(e);
+    } else {
+      this.saveContact($(e.target).serializeArray());
     }
     this.saveCurrentState();
     this.renderContacts();
+    this.filterByTag();
     this.hideForm();
   });
   $(document).on('click', '.add-contact', event => {
@@ -77,68 +101,71 @@ ContactManager.prototype.bind = function() {
     this.createContact();
   });
 
-  $(document).on('click.add-tag', '.add-tag', this.displayTagForm.bind(this));
-  $('.tags').on('click', 'a', e => {
+  $(document).on('click.add-tag', '.add-tag', event => {
+    event.preventDefault();
+    if ($UIElements.isFormActive()) { return; }
+    this.displayTagForm();
+  });
+  $UIElements.tagsContainer.on('click', 'a', e => {
     e.preventDefault();
     $(e.target).toggleClass('selected');
     this.filterByTag();
   });
-  $UIElements.search.on('input, keyup', this.filterContacts.bind(this));
-  $(document).on('click', 'input.cancel', this.cancel.bind(this));
-  $(document).on('blur', '#name, #email, #phone, #tag', this.update.bind(this));
   $(document).on('click', 'input.delete, input.edit', event => {
     event.preventDefault();
     let contactId = +$(event.target).parent().siblings(':hidden').val();
     this[$(event.target).val().toLowerCase()](contactId);
   });
+  $UIElements.search.on('input, keyup', this.filterContacts.bind(this));
+  $(document).on('click', 'input.cancel', this.cancel.bind(this));
 };
 
 ContactManager.prototype.displayFormFor = function(contact) {
   let context = { contact: contact, tags: this.tags };
   let $formContent = $(this.templates.contactFormTemplate(context));
+  $UIElements.contacts.removeClass('order-last');
   $UIElements.formContainer.html($formContent);
-  $('#contacts').toggleClass('collapsed');
+  $UIElements.contacts.toggleClass('collapsed');
   $UIElements.formContainer.toggleClass('collapsed');
 };
 
-ContactManager.prototype.displayTagForm = function(e) {
-  e.preventDefault();
+ContactManager.prototype.displayTagForm = function() {
   let $form = $(this.templates.tagFormTemplate());
   $UIElements.formContainer.html($form).toggleClass('collapsed');
   $UIElements.contacts.toggleClass('collapsed');
+
 }
 
 ContactManager.prototype.hideForm = function() {
+  $UIElements.contacts.addClass('order-last').toggleClass('collapsed');
   $UIElements.formContainer.toggleClass('collapsed');
-  $UIElements.contacts.toggleClass('collapsed');
-  // $UIElements.contacts.removeClass('order-last');
 };
 
 ContactManager.prototype.addTag = function(e) {
-  event.preventDefault();
+  e.preventDefault();
   let tag = $(event.target).find('#tagName').val();
   this.tags.push(tag);
-  $UIElements.tagsContainer.append(`<li><a class="button" href="#">${tag}</a></li>`)
+  $UIElements.addTag(tag)
 };
 
 ContactManager.prototype.getNextId = function() {
   return ++this.currentId;
 };
 
-ContactManager.prototype.createContact = function(e) {
+ContactManager.prototype.createContact = function() {
   let newContact = Object.create(contact);
-  newContact.id = this.getNextId();
-  this.contacts.push(newContact);
   this.displayFormFor(newContact);
+  $UIElements.setFormTitle('Create Contact');
 };
 
 // filters all contacts given a search string.
 ContactManager.prototype.filterContacts = function(event) {
+  event.preventDefault();
   if (this.contacts.length === 0) { return }
   let searchVal = $(event.currentTarget).val();
   if (searchVal.length === 0) {
     this.filterByTag();
-    $UIElements.searchEmptyMessage.hide();
+    $UIElements.searchEmptyMessage('');
     return;
   }
   let regExp = new RegExp(searchVal.replace(/[^a-z]/ig, ''), 'i');
@@ -147,18 +174,24 @@ ContactManager.prototype.filterContacts = function(event) {
     regExp.test($article.find('#contactName').text()) ? $article.show() : $article.hide();
   });
   if ($('article:visible').length === 0) {
-    $UIElements.setEmptyMessage();
+    $UIElements.searchEmptyMessage(searchVal);
   } else {
-    $UIElements.searchEmptyMessage.text('');
+    $UIElements.searchEmptyMessage('');
   }
 };
 
-ContactManager.prototype.filterByTag = function(event) {
-  let selectedTags = $('.selected').map((_, el) => $(el).text()).get();
+ContactManager.prototype.filterByTag = function() {
+  if (this.contacts.length === 0) { return;}
+  let selectedTags = $UIElements.getSelectedTagNames();
   this.contacts.forEach(contact => {
     let $article = $('article').filter((_, el) => $(el).find('input:hidden').val() === contact.id + '');
     selectedTags.indexOf(contact.tag) === -1 ? $article.hide() : $article.show();
   });
+  if ($('article:visible').length === 0) {
+    $UIElements.searchEmptyMessage(`${selectedTags.join(', ')}`);
+  } else {
+    $UIElements.searchEmptyMessage('');
+  }
 };
 
 ContactManager.prototype.getContact = function(contactId) {
@@ -174,19 +207,11 @@ ContactManager.prototype.delete = function (contactId) {
 ContactManager.prototype.edit = function (contactId) {
   let contact = this.getContact(contactId);
   this.displayFormFor(contact);
+  $UIElements.setFormTitle('Edit Contact');
 };
 
 ContactManager.prototype.cancel = function(e) {
-  let $form = $(event.target).closest('form');
-  if ($UIElements.getFormName().includes('Contact')) {
-    let contact = this.getContact(+$form.find('input:hidden').val());
-    let isContactComplete = $form.find(':empty').not('.button, :hidden').length === 0;
-    if (!isContactComplete) {
-      this.contacts.splice(this.contacts.indexOf(contact), 1);
-      if (this.currentId > 0) { this.currentId--; }
-    }
-  }
-  this.saveCurrentState();
+  e.preventDefault();
   this.hideForm();
 };
 
@@ -197,13 +222,6 @@ ContactManager.prototype.update = function(e) {
 
 ContactManager.prototype.saveCurrentState = function() {
   localStorage.setItem('manager', JSON.stringify(this));
-};
-let contact = {
-id: 0,
-name: '',
-email: '',
-phone: '',
-tag: ''
 };
 
 let manager= new ContactManager(JSON.parse(localStorage.getItem('manager')));
